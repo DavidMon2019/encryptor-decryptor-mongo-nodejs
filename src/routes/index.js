d063dbd1d65a76453ad4c28fbc8332710c0dc51b
David Monzón <dmonzonreyes@gmail.com>
2020-10-05 10:26:40 -0600

    la actualizacion encripta ahora la contraseña

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,62 +8,63 @@ const crypto = require('crypto'),
 
 
 router.get('/', async (req, res) => {
-  const user = await User.find();
-  res.render('index', {
-    user
-  });
+    const user = await User.find();
+    res.render('index', {
+        user
+    });
 });
 
 router.post('/add', async (req, res, next) => {
-  const user = new User(req.body);
-  const cipher = crypto.createCipher(algorithm,password)
-  let crypted = cipher.update(req.body.password,'utf8','hex')
-  crypted += cipher.final('hex');
-  user.password_aes = crypted;
+    const user = new User(req.body);
+    const cipher = crypto.createCipher(algorithm, password)
+    let crypted = cipher.update(req.body.password, 'utf8', 'hex')
+    crypted += cipher.final('hex');
+    user.password_aes = crypted;
 
-  await user.save();
-  console.log(user);
-  res.redirect('/');
+    await user.save();
+    console.log(user);
+    res.redirect('/');
 });
 
-function desencriptar(){
-  var decipher = crypto.createDecipher(algorithm,password)
-  var dec = decipher.update(req.body.password_aes,'hex','utf8')
-  dec += decipher.final('utf8');
-  document.getElementById('password').innerHTML = ' <textarea id="password" class="form-control" type="password" name="password" placeholder="Password" readonly >\n' +
-      '\n' +' doc'+
-      ' </textarea>';
+function desencriptar() {
+    var decipher = crypto.createDecipher(algorithm, password)
+    var dec = decipher.update(req.body.password_aes, 'hex', 'utf8')
+    dec += decipher.final('utf8');
+    document.getElementById('password').innerHTML = ' <textarea id="password" class="form-control" type="password" name="password" placeholder="Password" readonly >\n' +
+        '\n' + ' doc' +
+        ' </textarea>';
 }
 
-
-
-
 router.get('/turn/:id', async (req, res, next) => {
-  let { id } = req.params;
-  const user = await User.findById(id);
-  user.status = !user.status;
-  await user.save();
-  res.redirect('/');
+    let {id} = req.params;
+    const user = await User.findById(id);
+    user.status = !user.status;
+    await user.save();
+    res.redirect('/');
 });
 
-
 router.get('/edit/:id', async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-  console.log(user)
-  res.render('edit', { user });
+    const user = await User.findById(req.params.id);
+    console.log(user)
+    res.render('edit', {user});
 });
 
 router.post('/edit/:id', async (req, res, next) => {
-  const { id } = req.params;
-  await User.update({_id: id}, req.body);
-  res.redirect('/');
+    const {id} = req.params;
+    const cipher = crypto.createCipher(algorithm, password)
+    let crypted = cipher.update(req.body.password, 'utf8', 'hex')
+    crypted += cipher.final('hex');
+    req.body.password_aes = crypted;
+    console.log(req.body.password_aes);
+
+    await User.update({_id: id}, req.body);
+    res.redirect('/');
 });
 
 router.get('/delete/:id', async (req, res, next) => {
-  let { id } = req.params;
-  await User.remove({_id: id});
-  res.redirect('/');
+    let {id} = req.params;
+    await User.remove({_id: id});
+    res.redirect('/');
 });
-
 
 module.exports = router;
